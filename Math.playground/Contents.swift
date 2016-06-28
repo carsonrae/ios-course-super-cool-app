89eec50830d594a8c2960755fc8fd9d1582a3a72
Carson Rae <carsonrae6@gmail.com>
2016-06-28 11:22:53 -0700

    added files for supercool

---
+++ Math.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var firstNum = 15
+var secondNum = 5
+var sum = firstNum + secondNum
+var multipliedNum = firstNum * secondNum
+var dividedNum = firstNum/secondNum
+var arsenalsRanking = 20; var spursRanking = 1; var liverpoolRanking = 19
+let myNumber = 11; let myOrderOfBirth = 1; let myName = "Carson"
+var coolNumber: Double
+var coolerNumber: Int = 423
+var greeting: String = "Hello"
+var home: String = "World"
+var combo = greeting + " " + home
+combo = "\(greeting) \(home)"
